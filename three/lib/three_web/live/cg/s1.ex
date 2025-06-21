9d8d78c2bd554f2cdafa488ad9c5f2722a847b55
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-06-21 20:05:46 +0900

    デザイン調整

--- three/lib/three_web/live/cg/s1.ex
+++ three/lib/three_web/live/cg/s1.ex
@@ -3,7 +3,9 @@ defmodule ThreeWeb.CgLive.S1 do
   import ThreeWeb.Cg.CgHelper
 
   # 1列のパネル数
-  @plane_count 1_000
+  @plane_count 80
+  @plane_x -8
+  @plane_s 2
 
   @impl true
   def mount(_params, _session, socket) do
@@ -26,15 +28,15 @@ defmodule ThreeWeb.CgLive.S1 do
 
   @impl true
   def handle_info(:update, socket) do
-    Process.send_after(self(), :update, 12)
+    Process.send_after(self(), :update, 20)
     {:noreply, main(socket)}
   end
 
   def handle_event("load_model", %{"name" => "test", "status" => "completion"}, socket) do
     socket =
       socket
-      |> position("test", -2.5, -1, 1)
-      |> rotation("test", -3.5, 0, -1.6)
+      |> position("test", -2.5, 0, 1)
+      |> rotation("test", -2, 0, -1.6)
 
     {:noreply, socket}
   end
@@ -68,15 +70,15 @@ defmodule ThreeWeb.CgLive.S1 do
   end
 
   defp update(character_data) do
-    character_data + 0.05
+    if character_data >= 2, do: 0, else:  character_data + 0.01
   end
 
   defp add_planes(socket) do
     Enum.reduce(1..@plane_count, socket, fn x, acc ->
       acc
-      |> add_plane("bg_#{x}", 2, 2, "#666666")
-      |> add_plane("bg_a#{x}", 2, 2, "#666666")
-      |> add_plane("bg_b#{x}", 2, 2, "#666666")
+      |> add_plane("bg_#{x}", @plane_s, @plane_s, "#666666")
+      |> add_plane("bg_a#{x}", @plane_s, @plane_s, "#333333")
+      |> add_plane("bg_b#{x}", @plane_s, @plane_s, "#111111")
     end)
   end
 
@@ -89,10 +91,17 @@ defmodule ThreeWeb.CgLive.S1 do
   end
 
   defp positions(socket, add_x) do
+
     Enum.reduce(1..@plane_count, socket, fn x, acc ->
-      position(acc, "bg_#{x}", -100 + x * 2 + add_x * 8, -4, -2)
-      |> position("bg_a#{x}", -100 + x * 2 + add_x * 4, -2, -2)
-      |> position("bg_b#{x}", -100 + x * 2 + add_x * 2, 0, -2)
+      base = @plane_x  + x * @plane_s
+
+      bg_x =  base + add_x * 64
+      bg_ax =  base + add_x  * 16
+      bg_bx = base + add_x * 4
+
+      position(acc, "bg_#{x}", bg_x, -4, -2)
+      |> position("bg_a#{x}", bg_ax, -2, -2)
+      |> position("bg_b#{x}", bg_bx, 0, -2)
     end)
 
     # socket
